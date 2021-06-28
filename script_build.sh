d8ba4641e4138932b9835037f2aff84e8db2d8bf
Vjs Pranavasri <pranavasri@live.in>
2021-06-28 17:55:34 +0530

    Update script_build.sh

--- script_build.sh
+++ script_build.sh
@@ -1,101 +1,6 @@
 #!/bin/bash
-
-# curl https://raw.githubusercontent.com/vjspranav/RomBuildScript/ryzen7/script_build.sh>script_build.sh
-# Make necessary changes before executing script
-
-# Export some variables
-# make_clean (yes/no/installclean/deviceclean)
-user=
-lunch=
-device_codename=avicii
-build_type=userdebug
-tg_username=
+user=vjspranav
 OUT_PATH="out/target/product/$device_codename"
-use_ccache=yes
-make_clean=no
-stopped=0
-finish=0
-
-function finish {
-  stopped=1
-  rm -rf /tmp/manlocktest.lock;
-read -r -d '' msg <<EOT
-<b>Build Stopped</b>
-<b>Device:-</b> ${device_codename}
-<b>Started by:-</b> ${tg_username}
-EOT
-  if [ $finish = 0 ] ; then
-    telegram-send --format html "$msg" --config /ryzen.conf
-  fi
-}
-
-function setVar {
-    hours=$((i/3600))
-    minutes=$(( $((i/60)) - $((hours*60))))
-    seconds=$(( i - $((hours*60*60)) -  $((minutes*60))))
-    if [ $hours = 1 ]; then
-        h="Hour"
-    else
-       h="Hours"
-    fi
-    if [ $minutes = 1 ]; then
-        m="Minute"
-    else
-        m="Minutes"
-    fi
-    if [ $seconds = 1 ]; then
-        s="Second"
-    else
-        s="Seconds"
-    fi
-}
-
-i=0
-echo -n "Test Line might be deleted"
-while { set -C; ! 2>/dev/null > /tmp/manlocktest.lock; }; do
-  ((i=i+1))
-  uname2=$(ls -l /tmp/manlocktest.lock | awk '{print $3}');
-  setVar
-  if [ $uname2 = $USER ]; then
-        echo -e "Warning you can't wait while you are building"
-        exit 1
-  elif [ $i -gt 3600 ]; then
-        hours=$((i/3600))
-        minutes=$(( $((i/60)) - $((hours*60))))
-        seconds=$(( i - $((hours*60*60)) -  $((minutes*60))))
-        pr="$hours $h $minutes $m $seconds $s      "
-  elif [ $i -lt 60 ]; then
-        pr="$i $s  "
-  else
-        minutes=$((i/60))
-        seconds=$(( i - $((minutes*60))))
-        pr="$minutes $m $seconds $s    "
-  fi
-
-  echo -n -e "\r${uname2} Building. Waiting for $pr"
-  sleep 10
-done
-trap finish EXIT SIGINT
-
-echo -e "\rBuild starting thank you for waiting"
-
-
-#Start Counting build time after build started we don't want wait time included
-START=$(date +%s)
-BUILDFILE="buildlog"_$START.txt
-
-mkdir -p /home/${user}/downloads/buildlogs/
-touch /home/${user}/downloads/buildlogs/${BUILDFILE}
-BLINK="http://${user}.ryzenbox.me/buildlogs/${BUILDFILE}"
-# Send message to TG
-read -r -d '' msg <<EOT
-<b>Build Started</b>
-<b>Device:-</b> ${device_codename}
-<b>Started by:-</b> ${tg_username}
-<b>Console log:-</b> <a href="${BLINK}">here</a>
-EOT
-
-telegram-send --format html "$msg" --config /ryzen.conf
 
 # Colors makes things beautiful
 export TERM=xterm
@@ -106,8 +11,11 @@ export TERM=xterm
     cya=$(tput setaf 6)             #  cyan
     txtrst=$(tput sgr0)             #  Reset
 
+#Start Counting build time after build started we don't want wait time included
+START=$(date +%s)
+
 # Ccache
-if [ "$use_ccache" = "yes" ];
+if [ "$with_ccache" = "yes" ];
 then
 echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
 export CCACHE_EXEC=$(which ccache)
@@ -115,8 +23,7 @@ export USE_CCACHE=1
 export CCACHE_DIR=/home/$user/ccache
 ccache -M 75G
 fi
-
-if [ "$use_ccache" = "clean" ];
+if [ "$with_ccache" = "clean" ];
 then
 export CCACHE_EXEC=$(which ccache)
 export CCACHE_DIR=/home/$user/ccache
@@ -127,11 +34,8 @@ wait
 echo -e ${grn}"CCACHE Cleared"${txtrst};
 fi
 
-rm -rf ${OUT_PATH}/*.zip #clean rom zip in any case
-
-# Time to build
-source build/envsetup.sh
-lunch "$lunch"_"$device_codename"-"$build_type"
+#clean rom zip in any case
+rm -rf ${OUT_PATH}/*.zip
 
 # Clean build
 if [ "$make_clean" = "yes" ];
@@ -157,40 +61,10 @@ wait
 echo -e ${cya}"Device dir deleted from OUT dir"${txtrst};
 fi
 
-make bacon -j16 |& tee  "/home/${user}/downloads/buildlogs/${BUILDFILE}"
+# Build
+source build/envsetup.sh
+lunch ${lunch}_${device_codename}-${build_variabt}
+${make_command} -j16
 
 END=$(date +%s)
 TIME=$(echo $((${END}-${START})) | awk '{print int($1/60)" Minutes and "int($1%60)" Seconds"}')
-
-ROM=${OUT_PATH}/StagOS*.zip
-if [ -f $ROM ]; then
-
-cp $ROM /home/${user}/downloads/
-filename="$(basename $ROM)"
-LINK="http://${user}.ryzenbox.me/${filename}"
-read -r -d '' suc <<EOT
-<b>Build Finished</b>
-<b>Time:-</b> ${TIME}
-<b>Device:-</b> ${device_codename}
-<b>Started by:-</b> ${tg_username}
-<b>Download:-</b> <a href="${LINK}">here</a>
-EOT
-
-else
-
-# Send message to TG
-cp out/error.log /home/${user}/downloads/error.txt
-read -r -d '' suc <<EOT
-<b>Build Errored</b>
-<b>Time:-</b> ${TIME}
-<b>Device:-</b> ${device_codename}
-<b>Started by:-</b> ${tg_username}
-<b>Check error:-</b> <a href="http://${user}.ryzenbox.me/error.txt">here</a>
-EOT
-
-fi
-
-if [ $stopped = 0 ] ; then
-  telegram-send --format html "$suc" --config /ryzen.conf
-fi
-finish=1
