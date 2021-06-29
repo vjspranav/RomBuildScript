bae9721e875ea0d58fa99ac883abb13ea5f8ee85
Vjs Pranavasri <pranavasri@live.in>
2021-06-29 12:48:36 +0530

    Update script_build.sh

--- script_build.sh
+++ script_build.sh
@@ -20,13 +20,13 @@ then
 echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-export CCACHE_DIR=/home/$user/ccache
+export CCACHE_DIR=/var/lib/jenkins/$user/ccache
 ccache -M 75G
 fi
 if [ "$with_ccache" = "clean" ];
 then
 export CCACHE_EXEC=$(which ccache)
-export CCACHE_DIR=/home/$user/ccache
+export CCACHE_DIR=/var/lib/jenkins/$user/ccache
 ccache -C
 export USE_CCACHE=1
 ccache -M 75G
